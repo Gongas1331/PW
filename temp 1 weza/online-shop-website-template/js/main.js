31115abfb5382e7ddb8d5378dfe988755b382fa0
Gonçalo Cunha <goncalo.cunha@ua.pt>
2023-03-13 04:53:38 +0000

    merdas

--- temp 1 weza/online-shop-website-template/js/main.js
+++ temp 1 weza/online-shop-website-template/js/main.js
@@ -91,7 +91,7 @@
         if (button.hasClass('btn-plus')) {
             var newVal = parseFloat(oldValue) + 1;
         } else {
-            if (oldValue > 0) {
+            if (oldValue > 1) {
                 var newVal = parseFloat(oldValue) - 1;
             } else {
                 $(this).parent().parent().parent().parent().remove() ;
